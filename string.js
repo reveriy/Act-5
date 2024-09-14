50f3fb66c453feb1a4d4a63acef122e28d170852
Valorie Portem <valorieportem@gmail.com>
2024-09-14 23:14:38 +0800

    initial commit

---
+++ string.js
@@ -0,0 +1,17 @@
+let favActorFirstName = "Cillian";
+let favActorLastName = "Murphy";
+let fullName = favActorFirstName + " " + favActorLastName;
+
+let uppercase = fullName.toLowerCase();
+let lowercase = "Enero".toUpperCase();
+
+let message = "My favorite actor is" + " " + fullName.toLocaleUpperCase();
+
+message += ", his best show is Peaky BLinders.";
+
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(lowercase);
+console.log(message);
